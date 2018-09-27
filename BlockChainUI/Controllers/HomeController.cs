13da75e06b0d98d0f0c5f569a4b99837136b0898
Hanna Song <h-s93@hotmail.com>
2018-09-27 15:11:20 +1200

    Add project files.

---
+++ BlockChainUI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlockChainUI.Models;
+
+namespace BlockChainUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Nominate()
+        {
+            return View();
+        }
+        public IActionResult Vote()
+        {
+            return View();
+        }
+        public IActionResult Result()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
